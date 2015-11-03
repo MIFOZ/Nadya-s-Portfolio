944d099ef827367a8d1d0340542255e708af9631
Mifoz <efa34@yahoo.com>
2015-11-03 23:42:32 +0600

    Added create action to piece controller

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,7 +1,22 @@
 class PiecesController < ApplicationController
 
-def index
-  @piece = Piece.new
-end
+  def index
+    @piece = Piece.new
+    @pieces = Piece.all
+  end
+
+  def create
+    @piece = Piece.new(allowed_params)
+
+    if @piece.save
+      redirect_to pieces_path
+    else
+      renderer 'new'
+    end
+  end
+
+  def allowed_params
+    params.require(:piece).permit(:title, :image)
+  end
 
 end
